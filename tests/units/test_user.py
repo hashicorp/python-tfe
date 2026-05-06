1cde278af2a59dde4eca8e95e913d25d845e2834
Tanya Singh <Tanya.Singh369@ibm.com>
2026-05-06 14:23:20 +0530

    Fix import order in test_user

--- tests/units/test_user.py
+++ tests/units/test_user.py
@@ -4,6 +4,7 @@ import copy
 from unittest.mock import Mock
 
 import pytest
+ 
 from pytfe.models.user import User, UserPermissions, UserUpdateCurrentOptions
 from pytfe.resources.user import Users
 
