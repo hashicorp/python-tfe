68a29e1f1072e98ab21d02809e62b7ec0e0ad2ee
Tanya Singh <Tanya.Singh369@ibm.com>
2026-05-14 14:24:45 +0530

    fix: map task stage relationships in parser

--- src/pytfe/resources/task_stage.py
+++ src/pytfe/resources/task_stage.py
@@ -15,6 +15,28 @@ from ._base import _Service
 class TaskStages(_Service):
     """TaskStages provides access to task stage endpoints."""
 
+    def _parse_task_stage(self, data: dict[str, Any]) -> TaskStage:
+        attributes = data.get("attributes", {})
+
+        attributes["id"] = _safe_str(data.get("id"))
+
+        relationships = data.get("relationships", {})
+
+        run_data = relationships.get("run", {}).get("data")
+        attributes["run"] = run_data
+
+        task_results_data = relationships.get(
+            "task-results", {}
+        ).get("data", [])
+        attributes["task-results"] = task_results_data
+
+        policy_evaluations_data = relationships.get(
+            "policy-evaluations", {}
+        ).get("data", [])
+        attributes["policy-evaluations"] = policy_evaluations_data
+
+        return TaskStage.model_validate(attributes)
+
     # Read
     def read(self, task_stage_id: str) -> TaskStage:
         if not valid_string_id(task_stage_id):
@@ -26,10 +48,8 @@ class TaskStages(_Service):
         )
 
         data = response.json().get("data", {})
-        attributes = data.get("attributes", {})
-        attributes["id"] = _safe_str(data.get("id"))
 
-        return TaskStage.model_validate(attributes)
+        return self._parse_task_stage(data)
 
     # List
     def list(self, run_id: str) -> Iterator[TaskStage]:
@@ -39,10 +59,7 @@ class TaskStages(_Service):
         path = f"/api/v2/runs/{run_id}/task-stages"
 
         for item in self._list(path):
-            attributes = item.get("attributes", {})
-            attributes["id"] = item.get("id")
-
-            yield TaskStage.model_validate(attributes)
+            yield self._parse_task_stage(item)
 
     # Override
     def override(
@@ -62,7 +79,5 @@ class TaskStages(_Service):
         )
 
         data = response.json().get("data", {})
-        attributes = data.get("attributes", {})
-        attributes["id"] = _safe_str(data.get("id"))
 
-        return TaskStage.model_validate(attributes)
+        return self._parse_task_stage(data)
